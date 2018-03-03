d479ce60b56dfb950e4a0118e77738ad89fa26ce
Michael Gingras <mcg79@cornell.edu>
2018-03-03 18:49:25 -0500

    1234

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    // response: '',
+    info: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ info: res.info }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/info');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1 className="header--title">DEXTER</h1>
+          <h3 className="header--subtitle">Lightning Network Test</h3>
+        </header>
+        <div className="modal">
+          <span>Address <input type="text" /></span>
+          <span>Amount <input type="text" /></span>
+          <button>SEND</button>
+        </div>
+        <div className="container--body">
+          <p>Node Public Key <br/>
+            <span className="sub">{this.state.info}</span>
+          </p>
+          <p>Node Balance <br/>
+            <span className="sub">10000 Satoshis</span>
+          </p>
+          <p>Number of Active Channels <br/>
+            <span className="sub">0 Channels</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const grpc = require('grpc');
+const fs = require("fs");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const lndCert = fs.readFileSync('/Users/mcgingras/Library/Application Support/LND/tls.cert');
+const adminMacaroon = fs.readFileSync('/Users/mcgingras/Library/Application Support/LND/admin.macaroon');
+
+const meta = new grpc.Metadata();
+meta.add('macaroon', adminMacaroon.toString('hex'));
+process.env.GRPC_SSL_CIPHER_SUITES = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384";
+const credentials = grpc.credentials.createSsl(lndCert);
+
+const lnrpcDescriptor = grpc.load("rpc.proto");
+const lnrpc = lnrpcDescriptor.lnrpc;
+const lightning = new lnrpc.Lightning('localhost:10009', credentials);
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From h' });
+});
+
+app.get('/api/info', (req, res) => {
+  var _call = lightning.getInfo({}, meta, function(err, response) {
+      if (err) console.log(err);
+      if (response) res.send({ info: response.identity_pubkey});
+  });
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
